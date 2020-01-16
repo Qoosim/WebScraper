62e3d225e9608d60551e71cea940bce43d1bce09
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-16 14:28:24 +0100

    Rubygem 'httparty' and 'nokogiri' required

---
+++ lib/scraper.rb
@@ -0,0 +1,14 @@
+require 'httparty'
+require 'nokogiri'
+
+class Nike
+  attr_accessor :parsed_page
+
+  def initialize
+    content = HTTParty.get("https://www.nike.com/w/men-lifestyle-shoes-13jrmznik1zy7ok")
+    @parsed_page ||= Nokogiri::HTML(content)
+  end
+
+
+  
+end
