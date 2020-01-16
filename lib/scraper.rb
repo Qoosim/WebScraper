520ece47bb6ecd7987114956896a761f879d4bbd
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-16 17:05:02 +0100

    rubocop errors corrected

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,41 +5,40 @@ class Nike
   attr_accessor :parsed_page
 
   def initialize
-    content = HTTParty.get("https://www.nike.com/w/men-lifestyle-shoes-13jrmznik1zy7ok")
+    content = HTTParty.get('https://www.nike.com/w/men-lifestyle-shoes-13jrmznik1zy7ok')
     @parsed_page ||= Nokogiri::HTML(content)
   end
 
-  def get_names
-    names = []
+  def names
+    name = []
     items_container.each do |item|
-      names << item.css('.product-card__link-overlay').text
+      name << item.css('.product-card__link-overlay').text
     end
-    return names
+    name
   end
 
-  def get_prices
-    prices = []
+  def prices
+    price = []
     items_container.each do |item|
-      prices << item.css('.css-b9fpep').text
+      price << item.css('.css-b9fpep').text unless item.css('.css-b9fpep').nil?
     end
-    return prices
+    price
   end
 
   def print_output
-    names  = get_names
-    prices = get_prices
-    puts "====================================="
-    for i in 0..names.size - 1
-      puts "Name: #{names[i]}"
-      puts "Price: #{prices[i]}"
-      puts "====================================="
+    name = names
+    price = prices
+    puts '====================================='
+    (0..name.size - 1).each do |i|
+      puts "Name: #{name[i]}"
+      puts "Price: #{price[i]}"
+      puts '====================================='
     end
   end
 
   def items_container
     parsed_page.css('.product-grid__items').css('.product-grid__card')
   end
-
 end
 
 nike = Nike.new
